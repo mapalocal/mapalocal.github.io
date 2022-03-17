fb1c23d014ab81ce26277e042388496969ff5071
mapalocal <101777509+mapalocal@users.noreply.github.com>
2022-03-16 23:34:54 -0300

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,4 @@
+  var uid = '330666';
+   var wid = '653748';
+   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
